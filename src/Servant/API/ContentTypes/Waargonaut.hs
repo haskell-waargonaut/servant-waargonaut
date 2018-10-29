0f46399b61fbc75c62f3649e063fe6916d6ea451
Sean Chalmers <sclhiannan@gmail.com>
2018-10-29 13:49:26 +1000

    Update Waargonaut version to fix export issues.
    Documentation needed... Highlights issue in Waargonaut of possible problem RE strict / lazy
    bytestrings for parsers and decoder functions.

--- src/Servant/API/ContentTypes/Waargonaut.hs
+++ src/Servant/API/ContentTypes/Waargonaut.hs
@@ -4,31 +4,36 @@
 {-# LANGUAGE OverloadedStrings     #-}
 module Servant.API.ContentTypes.Waargonaut ( WaargJSON ) where
 
-import           Control.Applicative             ((<*))
-import           Control.Category                ((.))
-import           Control.Lens                    (over, _Left)
+import           Control.Applicative        ((<*))
+import           Control.Category           ((.))
+import           Control.Lens               (over, _Left)
 
-import           Prelude                         (show, unlines)
+import           Prelude                    (String, show, unlines)
 
-import           Data.Function                   (($))
-import           Data.Typeable                   (Typeable)
-import qualified Network.HTTP.Media              as M
-import qualified Data.List.NonEmpty as NE
+import qualified Data.ByteString.Lazy       as BSL
+import qualified Data.Text                  as Text
 
-import           Data.Attoparsec.ByteString.Lazy (eitherResult, endOfInput,
-                                                  parse)
+import           Data.Either                (Either)
+import           Data.Function              (($))
+import           Data.Typeable              (Typeable)
+
+import qualified Data.List.NonEmpty         as NE
+import qualified Network.HTTP.Media         as M
+
+import           Data.Attoparsec.ByteString (eitherResult, endOfInput, parse)
 
 
-import           Servant.API.ContentTypes        (Accept (..), MimeRender (..),
-                                                  MimeUnrender (..))
+import           Servant.API.ContentTypes   (Accept (..), MimeRender (..),
+                                             MimeUnrender (..))
 
-import           Waargonaut                      (parseWaargonaut)
-import           Waargonaut.Decode               (Err (..), ppCursorHistory,
-                                                  simpleDecode, unCursorHist)
-import           Waargonaut.Encode               (runPureEncoder)
+import           Waargonaut                 (parseWaargonaut)
+import           Waargonaut.Decode          (ppCursorHistory, simpleDecode)
+import           Waargonaut.Decode.Error    (DecodeError (ParseFailed))
 
-import           Waargonaut.Generic              (JsonDecode, JsonEncode,
-                                                  mkDecoder, mkEncoder)
+import           Waargonaut.Encode          (simplePureEncodeNoSpaces)
+
+import           Waargonaut.Generic         (JsonDecode, JsonEncode, mkDecoder,
+                                             mkEncoder)
 
 data WaargJSON deriving Typeable
 
@@ -36,15 +41,17 @@ instance Accept WaargJSON where
   contentTypes _ = "application" M.// "json" M./: ("charset", "utf-8") NE.:| [ "application" M.// "json" ]
 
 instance JsonDecode a => MimeUnrender WaargJSON a where
-  mimeUnrender _ = over _Left handleErr . simpleDecode parser mkDecoder
+  mimeUnrender _ = over _Left handleErr . simpleDecode mkDecoder parser . BSL.toStrict
     where
-      parser = eitherResult . parse (parseWaargonaut <* endOfInput)
+      parser =
+        over _Left (ParseFailed . Text.pack)
+        . eitherResult
+        . parse (parseWaargonaut <* endOfInput)
 
-      handleErr (Parse e)             = e
-      handleErr (Decode (dErr, hist)) = unlines
+      handleErr (dErr, hist) = unlines
         [ show dErr
         , show $ ppCursorHistory hist
         ]
 
 instance JsonEncode a => MimeRender WaargJSON a where
-  mimeRender _ = runPureEncoder mkEncoder
+  mimeRender _ = simplePureEncodeNoSpaces mkEncoder
