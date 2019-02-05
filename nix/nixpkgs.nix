f947af5d823a79797c02f99800f2ec3a4d867102
Sean Chalmers <sclhiannan@gmail.com>
2019-02-05 11:29:57 +1000

    Bump waargonaut release, remove tests from uri-bytestring in nix overlay, change bounds for waargonaut. Not tested with latest servant yet...

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,11 @@
-builtins.fetchGit {
-  # Descriptive name to make the store path easier to identify
-  name = "nixos-unstable-2018-10-10";
-  url = https://github.com/nixos/nixpkgs/;
-  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  rev = "0a7e258012b60cbe530a756f09a4f2516786d370";
-}
+let
+  hostNix = import <nixpkgs> {};
+  nixpkgsPin = hostNix.pkgs.lib.importJSON ./nixpkgs.json;
+
+  pinnedPkgs = hostNix.pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs-channels";
+    inherit (nixpkgsPin) rev sha256;
+  };
+in
+  pinnedPkgs
