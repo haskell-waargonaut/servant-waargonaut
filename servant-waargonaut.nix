326ddbfefff693c347b29e1b53f1e4888d1b4606
Sean Chalmers <sclhiannan@gmail.com>
2019-09-04 13:02:02 +1000

    Bump upper bounds of waargonaut and servant

--- servant-waargonaut.nix
+++ servant-waargonaut.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "servant-waargonaut";
-  version = "0.6.0.0";
+  version = "0.7.0.0";
   src = ./.;
   libraryHaskellDepends = [
     base bytestring http-media lens servant text waargonaut
